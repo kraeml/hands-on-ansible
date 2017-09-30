a040b0854ee3e736f1ac4db9149e02e762af77dc
vagrant <vagrant@example.com>
2017-09-30 13:23:31 +0000

    LXC-setup with automation login and documentation

--- 00-home-base/ais-lxc-setup.sh
+++ 00-home-base/ais-lxc-setup.sh
@@ -3,9 +3,12 @@
 -set x
 
 # Path for prepare playbook
-prepare=~/ais/hands-on-ansible/02-playbooks/00-simple-playbook-examples/prepare_ansible_target.yml
+PREPARE=~/ais/hands-on-ansible/02-playbooks/00-simple-playbook-examples/prepare_ansible_target.yml
+# Create ssh-key with empty passphrase (ONLY FOR TESTING)
+MAKE_SSH_KEY="ssh-keygen -b 2048 -t rsa -f ~/.ssh/id_rsa -q -N "" creates=~/.ssh/id_rsa"
 
-# Cnerate containers
+# Create containers
+# The login user is ubuntu with password ubuntu
 for i in ais-bashy web1 web2 db playbooks; do
 	sudo lxc-create -n ${i} -t ubuntu && \
 	# Remove legacy configuration keys \
@@ -23,9 +26,9 @@ for i in ais-bashy web1 web2 db playbooks; do
 		# Add ip into known_hosts
 		ssh-keyscan -H ${ip} >> ~/.ssh/known_hosts
 		# Setup ssh-key for user
-		ansible -i localhost, -m shell -a 'ssh-keygen -b 2048 -t rsa -f ~/.ssh/id_rsa -q -N "" creates=~/.ssh/id_rsa' --connection=local localhost
+		ansible -i localhost, -m shell -a "MAKE_SSH_KEY" --connection=local localhost
 		# Run prepare playbook
-		ansible-playbook -i ${ip}, -u ubuntu --ask-pass --ask-become-pass ${prepare}
+		ansible-playbook -i ${ip}, -u ubuntu --ask-pass --ask-become-pass ${PREPARE}
 	fi
 done
 sleep 3
