fe6c2faf14674f1172219a63156d7909f944a3ee
vagrant <vagrant@example.com>
2017-10-01 13:20:26 +0000

    Adding Notbook for 01-First-Steps

--- 01-first-steps/install-nginx.sh
+++ 01-first-steps/install-nginx.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 set -euo pipefail
+pwd
+cd "${BASH_SOURCE%/*}" || exit 
+pwd
 
 # Install nginx.
 apt-get update
@@ -9,16 +12,16 @@ apt-get install -y nginx sshguard nano curl # vim
 mkdir -p /var/www/mywebsite
 
 # Copy nginx config files (main config and vhost config)
-cp config/nginx.conf /etc/nginx/nginx.conf
+cp /tmp/config/nginx.conf /etc/nginx/nginx.conf
 chown root:root /etc/nginx/nginx.conf
 chmod 0644 /etc/nginx/nginx.conf
 
-cp config/mywebsite.conf /etc/nginx/conf.d/mywebsite.conf
+cp /tmp/config/mywebsite.conf /etc/nginx/conf.d/mywebsite.conf
 chown root:root /etc/nginx/conf.d/mywebsite.conf
 chmod 0644 /etc/nginx/conf.d/mywebsite.conf
 
 # Copy our website files
-cp config/index.html /var/www/mywebsite/
+cp /tmp/config/index.html /var/www/mywebsite/
 chown root:root /var/www/mywebsite/index.html
 chmod 0644 /var/www/mywebsite/index.html
 
@@ -26,13 +29,10 @@ chmod 0644 /var/www/mywebsite/index.html
 rm /etc/nginx/sites-enabled/default
 
 # Start nginx at boot + make sure it's running right now.
-service nginx restart
-service nginx enable
+systemctl restart nginx.service
+systemctl enabled nginx.service
 
 # On a machine with systemd, you'd use these instead
 # systemctl enable nginx
 # systemctl restart nginx
-
-
-
 
